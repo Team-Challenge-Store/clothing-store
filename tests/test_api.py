6589311ebad3a5023b49e6b592a91f27eccf3967
molfaryura <oleksandrr1996@protonmail.com>
2023-08-18 13:57:01 +0300

    Create two tests for the POST request

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,10 +1,33 @@
-import requests
+import logging
 
-url = 'http://127.0.0.1:5000/register'
-myobj = {'username': 'Ivan',
-         'email': 'ivan@gmail.com',
-         'password': '1234568'}
+from app import app
 
-x = requests.post(url, json = myobj)
 
-print(x.status_code == 201)
+def test_valid_post_request_to_register_user():
+    with app.test_client() as client:
+
+        data={'username': 'testing',
+         'email': 'testing@gmail.com',
+         'password': 'Tes123!',
+         'repeat_password': 'Tes123!'}
+        
+
+        response = client.post('/register', json=data)
+
+        logging.info(response.json['message'])
+
+        assert response.status_code == 201
+
+
+def test_unvalid_post_request_to_register_user():
+    with app.test_client() as client:
+
+        data={'username': '',
+         'email': '',
+         'password': '',
+         'repeat_password': ''}
+
+        response = client.post('/register', json=data)
+        logging.info(response.json)
+
+        assert response.status_code == 400
